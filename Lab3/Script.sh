9f9863418ce7ee8b04f4d395f4f8e75498b5a77c
thewamkov <thewankov@gmail.com>
2021-11-25 11:49:38 +0200

    Small fixes

--- Lab3/Script.sh
+++ Lab3/Script.sh
@@ -1,7 +1,7 @@
 
 
 # Find and print names of all soft-links on your VM
-find / -type l -exec ls -la '{}'
+find / -type l -exec ls -la '{}'\;
 
 # Find and print count of block and character devices
 find / -type b -or -type c | wc -l 
@@ -16,5 +16,6 @@ ln -s /etc/hostname /tmp/newhostname
 useradd testuser
 
 # Create file in home directory “testuser_data” owned by “testuser”
-touch /home/testuser/testuser_data
+touch /home/testuser_data
 chown testuser:testuser /home/testuser_data
+
