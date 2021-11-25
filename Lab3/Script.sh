b2e6400d2b02ad8bba4ea4844dfe42b2831d170b
thewamkov <thewankov@gmail.com>
2021-11-25 11:30:56 +0200

    'Renamed'

--- Lab3/Script.sh
+++ Lab3/Script.sh
@@ -1,26 +1,20 @@
 
-# create user “adminuser”
-useradd adminuser
 
-# set password for “adminuser”
- echo "linuxpassword" | passwd --stdin adminuser
+# Find and print names of all soft-links on your VM
+find / -type l -exec ls -la '{}'
 
-# Create group admin
-groupadd lnxadmin
+# Find and print count of block and character devices
+find / -type b -or -type c | wc -l 
 
-echo '%lnxadmin ALL=(ALL:ALL) ALL' >> /etc/sudoers
+# Find all folders with Sticky bit
+find / -type d -perm -1000 -exec ls -ld {} \;
 
-# Grant for “adminuser” sudoer permission
-usermod -a -G lnxadmin adminuser
+# Make soft link for /etc/hostname in /tmp folder
+ln -s /etc/hostname /tmp/newhostname
 
-# create user “poweruser”
-useradd poweruser
+# Create user “testuser”
+useradd testuser
 
-# grant for “poweruser” permission for iptables command
-echo 'poweruser ALL=(ALL:ALL) /usr/sbin/iptables' >> /etc/sudoers
-
-# Allow “poweruser” to read home directory of “adminuser” 
-usermod -a -G adminuser poweruser
-
-# List all of files with SUID bit set
-find / -type d -perm 4000 -exec ls -ld {} \;
+# Create file in home directory “testuser_data” owned by “testuser”
+touch /home/testuser/testuser_data
+chown testuser:testuser /home/testuser_data
