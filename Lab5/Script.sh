1cfec02ca9282392b9c6b87fae6d83797aa88cbe
thewamkov <thewankov@gmail.com>
2021-12-07 22:10:27 +0200

    Small Fixes

--- Lab5/Script.sh
+++ Lab5/Script.sh
@@ -14,9 +14,6 @@ systemctl start file-mover.service
 # Enable autostart
 systemctl enable file-mover.service
 
-
 # Change directory to cron.d
 cd /etc/cron.d
 
-# Every minute during working days log logged in users to the file
-echo '* * * * 1-5 root w>>/var/log/logged-in.log' >mycron
